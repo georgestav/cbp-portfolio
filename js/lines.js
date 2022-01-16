6d08e837c358f90d6794f209ef844157e56813c3
George Stavroulakis <g.stavroulakis@gmail.com>
2022-01-16 16:05:09 +0100

    implement lines, need to fix fade in

--- js/lines.js
+++ js/lines.js
@@ -1,22 +1,21 @@
-const container = document.getElementById("container");
-const svgContainer = document.getElementById("svgLines");
-
+const container = document.getElementById("svg");
+const svgLines = document.getElementById("svgLines");
 const listPoints = [];
 
 function genPosition() {
-	let pLeft = Math.floor(Math.random() * 1001);
-	let pTop = Math.floor(Math.random() * 501);
+	let pLeft = Math.floor(Math.random() * (container.clientWidth - 10));
+	let pTop = Math.floor(Math.random() * (container.clientHeight - 10));
 	listPoints.push([pLeft, pTop]);
-	return `style="position: absolute; left: ${pLeft}px; top: ${pTop}px;"`;
+	return `cx="${pLeft}" cy="${pTop}"`;
 }
 
-function genElement() {
-	let element = `<span class='dots' ${genPosition()}></span>`;
+function genDot() {
+	let element = `<circle class="dots fade-in-dots" ${genPosition()} r="4"/>`;
 	return element;
 }
 
 function draw() {
-	let point = container.insertAdjacentHTML("beforeend", genElement());
+	let point = svgLines.insertAdjacentHTML("beforeend", genDot());
 	return point;
 }
 
@@ -24,16 +23,19 @@ function line() {
 	listPoints.forEach((x, i, arr) => {
 		let arrayNoCurrent = arr.filter((value, index) => {
 			if (index !== i) {
-				let svgLine = `<line class="line" x1="${x[0]}" y1="${x[1]}" x2="${value[0]}" y2="${value[1]}"/>`;
-				svgContainer.insertAdjacentHTML("beforeend", svgLine);
+				let svgLine = `<line class="line fade-in-line" x1="${x[0]}" y1="${x[1]}" x2="${value[0]}" y2="${value[1]}"/>`;
+				svgLines.insertAdjacentHTML("afterbegin", svgLine);
 			}
 		});
 	});
 }
 
-line();
-
-let interval = setInterval(() => {
+var timesRun = 0;
+var interval = setInterval(function () {
+	timesRun++;
+	if (timesRun === 20) {
+		clearInterval(interval);
+	}
 	draw();
 	line();
 }, 3000);
