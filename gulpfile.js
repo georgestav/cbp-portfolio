47b47cae3c68949c161ee006192bcef6c6cb60b4
George Stavroulakis <g.stavroulakis@gmail.com>
2022-01-20 14:04:21 +0100

    team project

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,155 @@
-"use strict";
+// load modules
+const gulp = require("gulp");
+const del = require("del");
+const fs = require("fs");
+const browsersync_server = require("browser-sync").create();
+const htmlmin = require("gulp-htmlmin");
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require("gulp-autoprefixer");
+const csso = require("gulp-csso");
+const sourcemaps = require("gulp-sourcemaps");
 
-var gulp = require("gulp");
-var sass = require("gulp-sass")(require("sass"));
-var imagemin = require("gulp-imagemin");
+// creates default src folder structure
+function createStructure(done) {
+	const folders = ["dist", "src", "src/fonts", "src/img", "src/scss"];
+	const files = ["src/index.html"];
 
-//'buildStyles' function to convert scss to css, stores css to src directory
-function buildStyles() {
-	return gulp
-		.src("./src/scss/**/*.scss")
-		.pipe(sass().on("error", sass.logError))
-		.pipe(gulp.dest("./src/css"));
+	folders.forEach((dir) => {
+		if (!fs.existsSync(dir)) {
+			fs.mkdirSync(dir);
+			console.log("folder created:", dir);
+		}
+	});
+
+	files.forEach((file) => {
+		if (!fs.existsSync(file)) {
+			fs.writeFileSync(file, "");
+			console.log("file created:", file);
+		}
+	});
+
+	return done();
 }
 
-//'goPublic' function copies the source files to the distribution
-//prettier-ignore
-function goPublic(cb) {
-	(function () {
-		return [
-			gulp.src("./src/index.html").pipe(gulp.dest("./dist")), //copies the html to dist
-			gulp.src("./src/js/**/*.js").pipe(gulp.dest("./dist/js/")), // copies js to dist
-			gulp.src("./src/img/**/*.{jpg,png,svg,gif,ico,webp}").pipe(imagemin()).pipe(gulp.dest("./dist/images")), //copies imgs to dist
-			gulp.src("./src/css/*").pipe(gulp.dest("./dist/css")), // copy generated css to dist
-			gulp.src("./src/fonts/*").pipe(gulp.dest("./dist/fonts")), // copy generated css to dist
-		];
-	})();
-	cb();
+// deletes all assets (HTML, fonts, images) in dist
+function cleanAssets(done) {
+	return del(["dist/**/*.html", "dist/fonts/**/*", "dist/img/**/*"], {
+		force: true,
+	});
 }
 
-exports.goPublic = goPublic;
-exports.buildStyles = buildStyles;
+// publish HTML files src into dist
+function publishHtml(done, for_production = false) {
+	let pipeline = gulp.src("src/**/*.html");
 
-//'watch' function to watch for changes in the scss stylesheets and the html, calls the buildStyles that converts scss to css and goPublic that copies the src files to dist directory
-exports.watch = function () {
-	gulp.watch(
-		["./src/scss/**/*.scss", "./src/*.html", "./src/js/**/*.js"],
-		gulp.series(["buildStyles", "goPublic"])
-	);
-};
+	if (for_production) {
+		pipeline.pipe(htmlmin({ collapseWhitespace: true }));
+	}
+
+	return pipeline.pipe(gulp.dest("dist"));
+}
+
+// publish HTML for production
+function publishHtmlProduction(done) {
+	return publishHtml(done, true);
+}
+
+// publish HTML for development
+function publishHtmlDevelopment(done) {
+	return publishHtml(done, false);
+}
+
+// Copy all fonts from src/fonts into dist
+function publishFonts(done) {
+	return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
+}
+
+// Copy all images from src/img into dist
+function publishImages(done) {
+	return gulp.src("src/img/**/*").pipe(gulp.dest("dist/img"));
+}
+
+// compile SCSS files
+function compileScss(done, for_production = false) {
+	let pipeline = gulp
+		.src("src/scss/**/*.scss")
+		.pipe(sourcemaps.init())
+		.pipe(sass().on("error", sass.logError));
+
+	if (for_production) {
+		pipeline
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: ["last 2 version", "> 2%"],
+					cascade: false,
+				})
+			)
+			.pipe(csso());
+	}
+
+	return pipeline.pipe(sourcemaps.write(".")).pipe(gulp.dest("dist/css"));
+}
+
+function compileScssProduction(done) {
+	return compileScss(done, true);
+}
+
+function compileScssDevelopment(done) {
+	return compileScss(done, false);
+}
+
+// watch files
+function watchFiles(done) {
+	gulp.watch("src/**/*.html", gulp.series(publishHtml, reload));
+	gulp.watch("src/fonts/**/*", gulp.series(publishFonts, reload));
+	gulp.watch("src/img/**/*", gulp.series(publishImages, reload));
+	gulp.watch("src/scss/**/*.scss", gulp.series(compileScss, reload));
+}
+
+// browserSync server
+function serve(done) {
+	browsersync_server.init({
+		server: {
+			baseDir: "./dist/",
+		},
+	});
+	done();
+}
+
+// browserSync reload
+function reload(done) {
+	browsersync_server.reload();
+	done();
+}
+
+// export tasks
+exports.structure = createStructure;
+exports.publish = gulp.series(
+	cleanAssets,
+	publishHtml,
+	publishFonts,
+	publishImages
+);
+exports.build = gulp.series(
+	cleanAssets,
+	publishHtmlProduction,
+	publishFonts,
+	publishImages,
+	compileScssProduction
+);
+exports.build_dev = gulp.series(
+	cleanAssets,
+	publishHtmlDevelopment,
+	publishFonts,
+	publishImages,
+	compileScssDevelopment
+);
+exports.watch = gulp.series(
+	cleanAssets,
+	publishHtmlDevelopment,
+	publishFonts,
+	publishImages,
+	compileScssDevelopment,
+	serve,
+	watchFiles
+);
